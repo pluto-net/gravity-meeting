adcebb3a2dcf0161fcf8d02176fe3e2111d25a8d
SangHyuk Henry Jung <onlymytho@gmail.com>
2018-11-26 14:43:25 +0900

    Updated feature/New_Member_Care
    - New member will engage to all gravity-meeting.
    - New member criteria is customizable by NEW_MEMBER_CRITERIA

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 DEFAULT_MEETING_SIZE = 3  # Meeting engager
 NEW_MEMBER_CRITERIA = 15 # days
@@ -16,33 +16,63 @@ MEMBER_LIST = [ # [name, entered_at]
     ['YujeongNoh', '2018-11-26']
 ]
 
-def create_random_meeting(member_list, meeting_size = DEFAULT_MEETING_SIZE):
-    pool_size = len(member_list)
+# define member groups
+original_members = []
+new_members = []
 
-    meeting_groups = []
-    remained_size = pool_size
-    remained_members = member_list
 
-    while (True):
+def define_members(member_list):
+    # To sperate original/new members.
+    # New members are members who entered in 30 days.
+    
+    for m in member_list:
+        # define member variables
+        name = m[0]
+        entered_at = datetime.strptime(m[1], "%Y-%m-%d") # It should contain time too
+
+        # compare membrer's entered_at with today
+        new_members.append(name) if entered_at.date() > date.today() - timedelta(NEW_MEMBER_CRITERIA) else original_members.append(name)
+
+def create_random_meeting(meeting_size = DEFAULT_MEETING_SIZE):
+    # create meeting groups.
+    # meeting_size = pool_size_new + random_original_members
+
+    pool_size_original = len(original_members) # origi
+    pool_size_new = len(new_members)
+
+    meeting_group_list = []
+    remained_size = pool_size_original
+    remained_members = original_members
+    meeting_size = meeting_size - pool_size_new  # meeting_size reset including new_members
+    
+    while (True):    
         if remained_size > meeting_size:
+            # assign original_members to a meeting_group
             meeting_group = [remained_members[n] for n in random.sample(range(remained_size), meeting_size)]
             remained_size -= len(meeting_group)
             remained_members = [item for item in remained_members if item not in meeting_group]
-            meeting_groups.append(meeting_group)
+            
+            # assign new_members to the meeting_group
+            meeting_group += new_members
+
+            # record the meeting_group to meeting_group list
+            meeting_group_list.append(meeting_group)
         else:
             meeting_group = remained_members
-            meeting_groups.append(meeting_group)
+            meeting_group += new_members
+            meeting_group_list.append(meeting_group)
             break
-    return meeting_groups
+    return meeting_group_list
     
 if __name__== "__main__":
     meeting_size = DEFAULT_MEETING_SIZE
-    result = create_random_meeting(MEMBER_LIST, meeting_size=meeting_size)
+    define_members(MEMBER_LIST)
+    result = create_random_meeting(meeting_size=meeting_size)
 
     # print result
     print ("\n\nWelcome to gravity meeting: %s\n===========================================" % str(datetime.now()))
-    print ("\nTotal are %d members, and each group have %d members:\n" % (len(MEMBER_LIST), meeting_size))
-
+    print ("\nTotal are %d members, %d new members, and each group have %d members:\n" % (len(MEMBER_LIST), len(new_members), meeting_size))
+    print ("new_members: %s\n" % str(new_members))
     for m in result:
         print ("Organizer: @" + str(m[0]) + ", " + str(m))
     

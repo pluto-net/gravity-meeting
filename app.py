ad8cdb441a268c23d19e374e5489f68d3c4d3e41
SangHyuk Henry Jung <onlymytho@gmail.com>
2018-11-26 14:40:41 +0900

    Update member_list

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ import random
 from datetime import datetime
 
 DEFAULT_MEETING_SIZE = 3  # Meeting engager
-MEMBER_LIST = [
-    'sanghyuk.jung', 
-    'junseon', 
-    'shbaek', 
-    'tylor_shin',
-    'taeheon',
-    'myungho',
-    'hyeon',
-    'yoonji',
-    'yukyeong',
-    ]
+NEW_MEMBER_CRITERIA = 15 # days
+MEMBER_LIST = [ # [name, entered_at]
+    ['junseon', '2018-01-01'],
+    ['shbaek', '2018-01-01'], 
+    ['tylor_shin', '2018-01-01'],
+    ['taeheon', '2018-01-01'],
+    ['hyeon', '2018-01-01'],
+    ['yoonji', '2018-01-01'],
+    ['yukyeong', '2018-01-01'],
+    ['sanghyuk.jung', '2018-09-17'],
+    ['changbae', '2018-11-05'],
+    ['YujeongNoh', '2018-11-26']
+]
 
 def create_random_meeting(member_list, meeting_size = DEFAULT_MEETING_SIZE):
     pool_size = len(member_list)
